a225806d811e39f0b2112bae35049581836454f1
= <=>
2024-03-23 16:08:05 -0400

    Working 2

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,5 +2,10 @@ import React, { useState } from "react";
 
 export default function Cart({ cartData }) {
   console.log(cartData);
-  return <div className="cart">Items in Cart: {cartData.number}</div>;
+  return (
+    <div className="cart">
+      <div className="cartObj">{cartData.number}</div>
+      <img className="cartImg" src="../products/cart.jpg" alt="" />
+    </div>
+  );
 }
